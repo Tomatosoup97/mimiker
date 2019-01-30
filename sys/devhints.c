a90fb5196d51d7a882632956053f518ab4bc36f8
Mateusz Urbańczyk <mu@qed.ai>
2019-01-30 12:58:19 +0100

    Rewrite generic hinted child so that it uses devprops

--- sys/devhints.c
+++ sys/devhints.c
@@ -7,55 +7,51 @@
 
 extern uint8_t __malta_dtb_start[];
 
+device_t *get_or_create_device(device_t *bus) {
+  device_t *dev = NULL;
+  device_t *child;
+
+  TAILQ_FOREACH (child, &bus->children, link) {
+  // TODO: pass name, unit? how to recognize its the desired device?
+    if (1) {
+      dev = child;
+      // break;
+    }
+  }
+  if (dev == NULL) {
+    dev = make_device(bus, NULL);
+  }
+  return dev;
+}
+
 static void generic_hinted_child(device_t *bus, const void *fdt, int nodeoffset) {
     int proplen;
     char child_path[PATHBUF_LEN];
-    char child_path_as_device[PATHBUF_LEN];
     const fdt_property_t *prop_ptr;
-    // TODO: find driver here?
-    // driver_t *driver = NULL;
-    // TODO: name, unit will be assigned by devclass
-    // TODO: get or create device
-    // device_t *child = make_device(bus, driver);
-    // device_construct_fullpath(child, child_path_as_device, PATHBUF_LEN);
+    device_t *child = get_or_create_device(bus);
 
-    klog("child path as device: %s", child_path_as_device);
+    devprop_attr_val_t *path_attr = kmalloc(M_DEV, sizeof(devprop_attr_val_t), 0);
+    devprop_res_val_t *irq = kmalloc(M_DEV, sizeof(devprop_res_val_t), 0);
+    devprop_res_val_t *regs = kmalloc(M_DEV, sizeof(devprop_res_val_t), 0);
 
     fdt_get_path(fdt, nodeoffset, child_path, PATHBUF_LEN);
-    klog("full fdt path: %s", child_path);
+    path_attr->str_value = child_path;
+    set_device_prop_attr(child, FDT_PATH, path_attr);
 
     prop_ptr = fdt_getprop(fdt, nodeoffset, "interrupts", &proplen);
-    klog("Interrupts: %s, len: %d", prop_ptr->data, proplen);
+    irq->uint8_value = (uint32_t) prop_ptr->data; // cast to uint8_t ?
+    set_device_prop_res(child, IRQLINE, irq);
 
     prop_ptr = fdt_getprop(fdt, nodeoffset, "reg", &proplen);
-    klog("Regs: %s, len: %d", prop_ptr->data, proplen);
+    regs->uint8_value = (uint32_t) prop_ptr->data;
+    set_device_prop_res(child, IOPORT, regs);
 
-    // TODO: Get resources here from device hints (interrupts, regs)
-    // using fdt_getprop and then add them to ivars using
-    // bus_set_resource (not implemented yet)
-
-    /*
-    resource_int_value(name, unit, "port", &start);
-    resource_int_value(name, unit, "portsize", &count);
-    bus_set_resource(child, SYS_RES_IOPORT, 0, start, count);
-
-    start = 0;
-    count = 0;
-    resource_int_value(name, unit, "maddr", &start);
-    resource_int_value(name, unit, "msize", &count);
-
-    bus_set_resource(child, SYS_RES_MEMORY, 0, start, count);
-    bus_set_resource(child, SYS_RES_IRQ, 0, start, 1);
-    bus_set_resource(child, SYS_RES_DRQ, 0, start, 1);
-    */
     return;
 }
 
 void bus_enumerate_hinted_children(device_t *bus) {
     const void *fdt = (const void *)__malta_dtb_start;
-    // char bus_path_buff[PATHBUF_LEN];
-    // construct_device_path(bus, bus_path_buff, 256);
-    const char *path = "/pci@0/isa@1000";
+    char *path =  get_device_prop_attr(bus, FDT_PATH)->str_value;
 
     int bus_nodeoffset = fdt_path_offset(fdt, path);
     int child_nodeoffset;
